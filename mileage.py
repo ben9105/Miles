933037235798454441004533c9c684609c4c0aac
Ben <benjaminshew@gmail.com>
2018-09-10 19:55:38 -0700

    Add files via upload

---
+++ mileage.py
@@ -0,0 +1,6 @@
+print("How many kilometers did you or something")
+kms = input()
+miles = float(kms)/1.60934
+miles = round(miles, 2)
+print(f"Neat, I guess, you did a thing for {miles} miles")
+# round(thing to round, how many decimals)
